c398b23ebc903fd297b60121a9beb9332a13b736
stepping <michael.stepping@avinotec.de>
2019-10-30 20:17:55 +0100

    Copyright added

--- app/fhemobile/src/fhe/java/de/fhe/fhemobile/models/maps/MapsModel.java
+++ app/fhemobile/src/fhe/java/de/fhe/fhemobile/models/maps/MapsModel.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014 Fachhochschule Erfurt, Ernst-Abbe-Hochschule Jena
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.fhe.fhemobile.models.maps;
 
 import java.util.ArrayList;
@@ -6,9 +22,6 @@ import java.util.List;
 import de.fhe.fhemobile.vos.maps.MapCollectionVo;
 import de.fhe.fhemobile.vos.maps.MapVo;
 
-/**
- * Created by paul on 23.02.14.
- */
 public class MapsModel {
 
     public static MapsModel getInstance() {
@@ -25,23 +38,23 @@ public class MapsModel {
     private MapsModel() {
         mMaps = new ArrayList<>();
 
-        List<MapVo> placesList = new ArrayList<>();
+        final List<MapVo> placesList = new ArrayList<>();
         placesList.add(new MapVo("Standorte FH Erfurt", "map_standorte.png"));
         MapCollectionVo places = new MapCollectionVo("Standorte FH Erfurt", placesList);
 
-        List<MapVo> altonaerList = new ArrayList<>();
+        final List<MapVo> altonaerList = new ArrayList<>();
         altonaerList.add(new MapVo("Campus Altonaer Straße", "map_campus.png"));
         MapCollectionVo altonaer = new MapCollectionVo("Campus Altonaer Straße", altonaerList);
 
-        List<MapVo> steinplatzList = new ArrayList<>();
+        final List<MapVo> steinplatzList = new ArrayList<>();
         steinplatzList.add(new MapVo("Steinplatz 2", "map_steinplatz.png"));
         MapCollectionVo steinplatz = new MapCollectionVo("Steinplatz 2", steinplatzList);
 
-        List<MapVo> leipzigerList = new ArrayList<>();
+        final List<MapVo> leipzigerList = new ArrayList<>();
         leipzigerList.add(new MapVo("Leipziger Straße 77", "map_leipziger.png"));
         MapCollectionVo leipziger = new MapCollectionVo("Leipziger Straße 77", leipzigerList);
 
-        List<MapVo> schlueterList = new ArrayList<>();
+        final List<MapVo> schlueterList = new ArrayList<>();
         schlueterList.add(new MapVo("Schlüterstraße 1", "map_schlueter.png"));
         MapCollectionVo schlueter = new MapCollectionVo("Schlüterstraße 1", schlueterList);
 
