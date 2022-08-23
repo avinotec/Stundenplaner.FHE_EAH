1ff59ec5108b2f7887a7646635b835222c7e94b3
stepping <michael.stepping@avinotec.de>
2022-08-23 11:29:19 +0200

    javadoc added

--- app/fhemobile/src/main/java/de/fhe/fhemobile/utils/navigation/AStar.java
+++ app/fhemobile/src/main/java/de/fhe/fhemobile/utils/navigation/AStar.java
@@ -334,6 +334,9 @@ public class AStar {
     private static class CellComparator implements Comparator<Cell> {
 
         @Override
+        /**
+         * @return int the value 0 if x == y; a value less than 0 if x < y; and a value greater than 0 if x > y
+         */
         public int compare(final Cell cellOne, final Cell cellTwo) {
             return Integer.compare(cellOne.getCostsPathToCell(), cellTwo.getCostsPathToCell());
         }
