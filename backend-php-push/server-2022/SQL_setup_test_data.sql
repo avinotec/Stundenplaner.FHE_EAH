f8dcf9c68fdc6e5fa306ce2f0c75658091f60751
nagorczak <nagorczak@online.de>
2022-12-01 12:42:18 +0100

    set up sql with test data

---
+++ backend-php-push/server-2022/SQL_setup_test_data.sql
@@ -0,0 +1,29 @@
+
+-- ------------------------------------------------- SET UP SCENARIO 1 -------------------------------------------------
+
+-- Insert subscription with my test phone's firebase token
+-- to test getting notifications
+INSERT INTO `fcm_user`(`token`, `eventseries_name`, `os`, `language`)
+VALUES ('cjvV3gR0S8CrHdGeiit35A:APA91bG2TVxTW-HTh8IL5_EKCpodm0PhlBQR98I-NaeYqgnB-LoJUj1SKsIjsumywPcEJXzsVUlus5Lx6wz4Bi8k0eQ7KoeP_Plsl8I_AmgdrVgaFuzPYTZYCz_fUINh-B96HSTqQRRE',
+        'BT/MT/WT(BA)Biomat./V/01','0','DE');
+
+-- Insert an event set belonging to the test subscription,
+-- the entry must be outdated to force a timetable change being detected
+INSERT INTO `event_sets`(`eventset_id`, `eventseries`, `module_id`, `eventset_data`)
+VALUES ('B7D7E57FE89C0A1FEDE9F9BA762D67F8','BT/MT/WT(BA)Biomat./V/01','0B0EA277617D8B137013B76C0C187E99','{}')
+ON DUPLICATE KEY UPDATE eventset_data = '{}';
+
+-- Insert the test subscription with another token
+-- to test sending notifications to multiple devices
+INSERT INTO `fcm_user`(`token`, `eventseries_name`, `os`, `language`)
+VALUES ('token0815','BT/MT/WT(BA)Biomat./V/01','0','DE');
+
+
+-- ------------------------------------------------- SET UP SCENARIO 2 -------------------------------------------------
+-- Test sending notifications for subscriptions made via app
+
+-- First, subscribe an event series using the app on the test device
+-- Then, insert/change an event set of the event series to force a timetable change being detected
+INSERT INTO `event_sets`(`eventset_id`, `eventseries`, `module_id`, `eventset_data`)
+VALUES ('B7D7E57FE89C0A1FEDE9F9BA762D67F8','BT/MT/WT(BA)Biomat./V/01','0B0EA277617D8B137013B76C0C187E99','{}')
+ON DUPLICATE KEY UPDATE eventset_data = '{}';
