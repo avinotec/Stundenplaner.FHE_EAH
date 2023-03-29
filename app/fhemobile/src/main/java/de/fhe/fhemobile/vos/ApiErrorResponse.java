5db3de122d962f6ed96e21a74bbf2790fae5ec74
stepping <michael.stepping@avinotec.de>
2023-03-29 14:49:23 +0200

    jump to today commented out, currently not used

--- app/fhemobile/src/main/java/de/fhe/fhemobile/vos/ApiErrorResponse.java
+++ app/fhemobile/src/main/java/de/fhe/fhemobile/vos/ApiErrorResponse.java
@@ -4,16 +4,20 @@ import com.google.gson.annotations.SerializedName;
 
 public class ApiErrorResponse {
 
-    public String getMessage() {
-        return message;
-    }
+// --Commented out by Inspection START (29.03.2023 02:13):
+//    public String getMessage() {
+//        return message;
+//    }
+// --Commented out by Inspection STOP (29.03.2023 02:13)
 
     public int getId(){
         return id;
     }
 
-    @SerializedName("message")
-    private String message;
+// --Commented out by Inspection START (29.03.2023 02:14):
+//    @SerializedName("message")
+//    private String message;
+// --Commented out by Inspection STOP (29.03.2023 02:14)
 
     @SerializedName("id")
     private int id;
