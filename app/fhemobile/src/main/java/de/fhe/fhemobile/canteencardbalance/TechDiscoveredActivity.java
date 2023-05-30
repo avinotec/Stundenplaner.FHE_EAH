57b13182adf624a0cba5264bd91a6f344971d10f
nagorczak <nagorczak@online.de>
2023-05-30 12:52:28 +0200

    added files of canteen card balance library

---
+++ app/fhemobile/src/main/java/de/fhe/fhemobile/canteencardbalance/TechDiscoveredActivity.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2016 Heinrich Reimer
+ *
+ * Authors:
+ * Heinrich Reimer <heinrich@heinrichreimer.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.fhe.fhemobile.canteencardbalance;
+
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+
+import de.fhe.fhemobile.canteencardbalance.canteencardreader.CardBalance;
+import de.fhe.fhemobile.canteencardbalance.canteencardreader.InterCardReader;
+import de.fhe.fhemobile.canteencardbalance.canteencardreader.desfire.DesfireException;
+
+public class TechDiscoveredActivity extends AppCompatActivity {
+
+    private static final String DEBUG_TAG = "TechDiscoveredActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(getIntent().getAction())) {
+            onNewIntent(getIntent());
+        }
+        else {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            try {
+                CardBalance balance = InterCardReader.getInstance().readTag(tag);
+                if (balance != null) {
+                    Log.d(DEBUG_TAG, balance.toString());
+
+                    Intent broadcast = new Intent(CardBalance.ACTION_CARD_BALANCE);
+                    broadcast.putExtras(balance.toBundle());
+                    sendBroadcast(broadcast);
+                }
+            } catch (DesfireException ignored) {
+                // Card is not supported
+            }
+        }
+
+        finish();
+    }
+}
