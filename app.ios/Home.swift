46441324c434e0deaf6030a0b35dc4af82c0d6d3
marvox99 <134492465+marvox99@users.noreply.github.com>
2023-07-17 23:54:34 +0200

    Add files via upload

--- app.ios/Home.swift
+++ app.ios/Home.swift
@@ -91,65 +91,124 @@ struct SemesterData: Codable {
     let studentsetData: [String: StudentData]
 }
 
-struct StudentData: Codable {
+struct StudentData: Codable, Hashable{
     let studentsetId: String
     let studentsetName: String
     let studentsetNumber: String
 }
-
-struct Stundenplan: View{
-    
-    @State private var studentsetNames: [String] = []
-    
-    var body: some View {
-        NavigationView{          List(studentsetNames, id: \.self) { studentsetName in
-            Text(studentsetName)
-        }
-        .navigationTitle("Stundenplan")
-        .onAppear {
-            fetchStudentsetData()
-        }
+struct ActivitySchedule {
+    struct Activity {
+        let activityId: String
+        let activityName: String
+        let activityDurationInMinutes: Int
+        let activityTypName: String
+        let activityTypDescription: String
+        let startDateTime: Int
+        let endDateTime: Int
+        let moduleId: String
+        let moduleName: String
+        let dataStaff: [String: Staff]
+        let dataLocation: [String: Location]
     }
+    
+    struct Staff {
+        let staffId: String
+        let staffName: String
+    }
+    
+    struct Location {
+        let locationId: String
+        let locationName: String
+    }
+    
+    struct DataDay {
+        let dayNumber: Int
+        let dayStart: Int
+        let dataActivity: [String: Activity]
+    }
+    
+    let weekStart: Int
+    let weekEnd: Int
+    let weekNumber: Int
+    let dataDay: [String: DataDay]
 }
+struct Stundenplan: View {
+    @State private var studentsetData: [String: StudentsetData] = [:]
+    @State private var studentsetNames: [String] = []
+    @State private var selectedLongname: String = ""
+    @State private var selectedSetgruppen: [String] = []
 
-func fetchStudentsetData() {
-    let urlString = "https://stundenplanung.eah-jena.de/api/mobileapp/v1/studentset/list"
-    guard let url = URL(string: urlString) else {
-        print("URL not available")
-        return
+    @State private var selectedSemesterData: [String: SemesterData] = [:]
+
+    var body: some View {
+        NavigationView {
+            NavigationStack {
+                List(studentsetNames.sorted(by: { studentsetName1, studentsetName2 in
+                    let name1 = studentsetData[studentsetName1]?.stgNamelong ?? ""
+                    let name2 = studentsetData[studentsetName2]?.stgNamelong ?? ""
+                    return name1 < name2
+                }), id: \.self) { studentsetName in
+                    if let studentset = studentsetData[studentsetName] {
+                        Button(action: {
+                            selectedLongname = studentset.stgNamelong
+                            selectedSetgruppen = Array(studentset.semesterData.keys)
+                            selectedSemesterData = studentset.semesterData // Update selected semesterData here
+                        }) {
+                            VStack(alignment: .leading) {
+                                Text("\(studentset.stgNamelong) - \(studentset.stgNameshort)")
+                                    .font(.headline)
+                                Text(studentsetName)
+                            }
+                        }
+                    }
+                }
+                .navigationTitle("Stundenplan")
+
+                List(selectedSetgruppen, id: \.self) { selectedSetgruppe in
+                    if let semesterData = selectedSemesterData[selectedSetgruppe] {
+                        ForEach(semesterData.studentsetData.values.sorted(by: { $0.studentsetName < $1.studentsetName }), id: \.self) { studentData in
+                            Text(studentData.studentsetNumber)
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                fetchStudentsetData()
+            }
+        }
     }
-    
-    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-        if let error = error {
-            print("Error: \(error)")
+    func fetchStudentsetData() {
+        let urlString = "https://stundenplanung.eah-jena.de/api/mobileapp/v1/studentset/list"
+        guard let url = URL(string: urlString) else {
+            print("URL not available")
             return
         }
-        
-        if let data = data {
-            decodeJSON(jsonData: data)
+
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+
+            if let data = data {
+                decodeJSON(jsonData: data)
+            }
+        }
+
+        task.resume()
+    }
+
+    func decodeJSON(jsonData: Data) {
+        do {
+            let responseData = try JSONDecoder().decode(ResponseData.self, from: jsonData)
+            studentsetData = responseData.studentset
+            studentsetNames = Array(responseData.studentset.keys)
+        } catch {
+            print("JSON decoding error: \(error)")
         }
     }
-    
-    task.resume()
 }
 
-func decodeJSON(jsonData: Data) {
-    do {
-        let responseData = try JSONDecoder().decode(ResponseData.self, from: jsonData)
-        
-        var studentsetNames: [String] = []
-        for (_, studentsetData) in responseData.studentset {
-            studentsetNames.append(studentsetData.stgNamelong)
-        }
-        
-        DispatchQueue.main.async {
-            self.studentsetNames = studentsetNames
-        }
-    } catch {
-        print("Error decoding JSON: \(error)")
-    }
-}
-}
 struct Thoska: View{
        var body: some View {
         NavigationView{
@@ -227,15 +286,80 @@ struct Thoska: View{
         .navigationBarTitleDisplayMode(.inline)
     }
 }
-    struct Mensaplan: View{
-        
-        var body: some View {
-            NavigationView{
-                
-                Text("Gerichte")
+struct Meal: Codable,Hashable {
+    let title: String
+    let description: String
+    let ingredients: String
+    let price: String
+    let date: Int64
+    let dateAsString: String
+    let mensaName: String
+    let mensaId: String
+}
+
+struct Mensaplan: View {
+    @State private var meals: [Meal] = []
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(meals, id: \.self) { meal in
+                    if shouldShowDate(meal) {
+                        Text(meal.dateAsString)
+                            .bold()
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("\(meal.title) - \(meal.description)")
+                        Text("Studierende/ Bedienstete/ Gäste")
+                        Text(meal.price)
+                    }
+                }
+            }
+            .navigationTitle("Mensa CARL-ZEISS-PROMENADE")
+            .onAppear {
+                fetchMealdata()
             }
         }
     }
+            private func shouldShowDate(_ meal: Meal) -> Bool {
+                  let previousIndex = meals.firstIndex(of: meal)! - 1
+                  return previousIndex < 0 || meal.dateAsString != meals[previousIndex].dateAsString
+              }
+    func fetchMealdata() {
+        let urlString = "http://app.fh-erfurt.de:8080/fheapp/api/eah/canteens/58"
+        guard let url = URL(string: urlString) else {
+            print("URL not available")
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+
+            if let data = data {
+                decodeWJSON(jsonData: data)
+            }
+        }
+
+        task.resume()
+    }
+
+    func decodeWJSON(jsonData: Data) {
+        do {
+            let responseData = try JSONDecoder().decode([Meal].self, from: jsonData)
+
+            DispatchQueue.main.async {
+                self.meals = responseData
+            }
+        } catch {
+            print("Error decoding JSON: \(error)")
+        }
+    }
+}
+
 
 struct WeatherData: Codable, Hashable {
     let temperature: String
